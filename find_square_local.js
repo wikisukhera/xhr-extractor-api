647ea8c6e75418809c8b81fd710fe285ede6eaaa
wikisukhera <wikisukhera@gmail.com>
2025-11-06 18:18:43 +0500

    Refactor puppeteer launch options in findSquare

--- find_square_local.js
+++ find_square_local.js
@@ -6,11 +6,11 @@
 const puppeteer = require('puppeteer-core');
 
 async function findSquare(address) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/tmp/chrome-linux/chrome'
-  });
+const browser = await puppeteer.launch({
+  headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+  executablePath: '/tmp/chrome-linux/chrome'
+});
 
   const page = await browser.newPage();
 
