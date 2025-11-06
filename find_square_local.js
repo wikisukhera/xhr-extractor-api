f48f179c7d7f5134e74419c81329d4443d6458f0
wikisukhera <wikisukhera@gmail.com>
2025-11-06 19:37:43 +0500

    Add web security flags and dynamic Chrome path

--- find_square_local.js
+++ find_square_local.js
@@ -8,8 +8,15 @@ const puppeteer = require('puppeteer-core');
 async function findSquare(address) {
 const browser = await puppeteer.launch({
   headless: true,
-  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
-  executablePath: '/tmp/chrome-linux/chrome'
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-web-security',
+    '--allow-running-insecure-content'
+  ],
+  executablePath: process.env.CHROME_PATH || '/tmp/chrome-linux/chrome'
 });
 
   const page = await browser.newPage();
